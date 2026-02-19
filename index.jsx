62992da0175b02d4600552a36e522badba93851c
Destin <destinusmom005@gmail.com>
2026-02-19 11:16:02 +0700

    initial commit

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 export default function NeonText() {
   return (
     <div className="neon-container">
-      <h1 className="neon-text">NEON LIGHT</h1>
+      <h1 className="neon-text">MENYALA ABANGKUH</h1>
     </div>
   );
 }
